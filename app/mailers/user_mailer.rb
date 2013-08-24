1d5cd11dbedc42100f04190bfe9f9c6c114bc3dd
Tom Whyntie <t.whyntie@qmul.ac.uk>
2013-08-24 04:15:17 +0000

    Added basic production deployment info.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,44 @@
+require 'uri'
+
 class UserMailer < ActionMailer::Base
-  default :from => "no-reply@#{host}"
 
   def forgot_password(user, key)
     @user, @key = user, key
-    mail( :subject => "#{app_name} -- forgotten password",
+	logger.info
+	logger.info "FORGOTTEN PASSWORD - email: #{@user.email_address}"
+	logger.info
+	logger.info "Dear #{@user.name},"
+	logger.info
+	logger.info "Please paste the link below into your browser. This will let you reset your password."
+	logger.info
+	#u = URI.parse(url_for(@user))
+	#logger.info u.scheme + "://" + u.host + "/DAQMAP" + u.path + "/reset_password_from_email/#{@key}"
+	logger.info url_for(@user) + "/reset_password_from_email/#{@key}"
+	logger.info
+	logger.info "Many thanks, the CERN@school DAQMAP team."
+	logger.info
+    mail( :subject => "The #{app_name} -- forgotten password",
           :to      => user.email_address )
   end
 
   def invite(user, key)
     @user, @key = user, key
-    mail( :subject => "Invitation to #{app_name}",
+	logger.info
+	logger.info "NEW USER - email #{@user.email_address}"
+	logger.info
+	logger.info "Dear #{@user.name},"
+	logger.info
+	logger.info "You have been invited to join the CERN@school DAQMAP!"
+	logger.info
+	logger.info "Please paste the link below into your browser to accept the invitation:"
+	logger.info
+	#u = URI.parse(url_for(@user))
+	#logger.info u.scheme + "://" + u.host + "/DAQMAP" + u.path + "/accept_invitation_from_email/#{@key}"
+	logger.info url_for(@user) + "/accept_invitation_from_email/#{@key}"
+	logger.info
+	logger.info "Many thanks, the CERN@school DAQMAP team."
+	logger.info
+    mail( :subject => "Invitation to the #{app_name}",
           :to      => user.email_address )
   end
 
